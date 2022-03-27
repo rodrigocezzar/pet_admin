7f1e2886a99d858eb54bc7c38dc843fc5aeebef5
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-03-26 22:56:41 -0300

    feat/create migrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :products
+    resources :sells
+    resources :suppliers
+    resources :campaigns
+    resources :discounts
+    resources :services
+    resources :products
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_003919) do
+ActiveRecord::Schema.define(version: 2022_03_27_015403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "observations"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "title"
+    t.decimal "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "fae_changes", id: :serial, force: :cascade do |t|
     t.integer "changeable_id"
@@ -183,4 +206,49 @@ ActiveRecord::Schema.define(version: 2022_03_27_003919) do
     t.index ["unlock_token"], name: "index_fae_users_on_unlock_token", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.decimal "price"
+    t.text "observations"
+    t.bigint "supplier_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supplier_id"], name: "index_products_on_supplier_id"
+  end
+
+  create_table "sells", force: :cascade do |t|
+    t.decimal "total"
+    t.bigint "discount_id"
+    t.bigint "client_id"
+    t.text "observations"
+    t.integer "status"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_sells_on_client_id"
+    t.index ["discount_id"], name: "index_sells_on_discount_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+    t.text "observations"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.text "observations"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "suppliers"
+  add_foreign_key "sells", "clients"
+  add_foreign_key "sells", "discounts"
 end
