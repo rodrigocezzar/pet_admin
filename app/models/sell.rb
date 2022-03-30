ce2cdaf5abb10ee9c8578ac6fe17718e35f57fe0
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-03-30 16:08:08 -0300

    feat/created a model for the relationship between products and sales

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -2,6 +2,14 @@ class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
   validates :client, presence: true
+
+  belongs_to :discount
+  belongs_to :client
+  has_many :sell_products
+  has_many :products, through: :sell_products
+  
+  has_many :sell_services
+  has_many :services, through: :sell_services
  
   def fae_display_field
     id
@@ -10,7 +18,4 @@ class Sell < ApplicationRecord
   def self.for_fae_index
     order(:id)
   end
- 
-  belongs_to :discount
-  belongs_to :client
 end
